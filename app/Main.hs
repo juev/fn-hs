7548a90864557f52b2f75c91a453ac6d865afdaf
Denis Evsyukov <denis@evsyukov.org>
2020-10-11 11:52:02 +0300

    [+] getGitSHADigest

--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,17 @@
 
 module Main where
 
-import           Data.Time                 (ZonedTime, defaultTimeLocale,
-                                            formatTime, getZonedTime)
--- import System.Process (system)
 import           Data.ByteString.Lazy.UTF8 as BSU (fromString)
 import           Data.Digest.Pure.SHA      (sha256, showDigest)
+import           Data.Time                 (ZonedTime, defaultTimeLocale,
+                                            formatTime, getZonedTime)
 import           System.Directory          (getCurrentDirectory)
+-- import           System.Exit
+import           System.Process
 import           Text.Printf               (printf)
+
 shaDigestLength = 8
+gitSHAsize = 8
 
 type Mill = Bool
 
@@ -17,9 +20,10 @@ main :: IO ()
 main = do
     currentDirectory <- getCurrentDirectory
     putStrLn $ "Current directory: " ++ currentDirectory
+    gitSHADigest <- getGitSHADigest
     now <- getZonedTime
     let dataTime = getTime now False in
-        printf "Result: %s--%s" dataTime . shaDigest $ dataTime
+        printf "Result: %s-%s-%s" dataTime gitSHADigest . shaDigest $ dataTime
 
 getTime :: ZonedTime -> Mill -> String
 getTime now True  = formatTime defaultTimeLocale "%Y%m%d-%H%M%S_%-q" now
@@ -27,3 +31,11 @@ getTime now False = formatTime defaultTimeLocale "%Y%m%d-%H%M%S" now
 
 shaDigest :: String -> String
 shaDigest = take shaDigestLength . showDigest . sha256 . BSU.fromString
+
+getGitSHADigest :: IO String
+getGitSHADigest = do
+    ( _, output, _ ) <- readProcessWithExitCode "git" ["rev-parse", "--short=8", "HEAD"] []
+    return $ removeEndOfLine output
+
+removeEndOfLine :: String -> String
+removeEndOfLine = filter (/= '\n')
